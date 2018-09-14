a0b442631fc32c373ae856eb1e55df0b921e5c6d
michel adriano medeiros <michel.medeiros@ti.rc.sp.gov.br>
2018-09-14 09:14:16 -0300

    first commit

---
+++ AppMountComputer/AppMountComputer/Paginas/CadastroProcessador.xaml.cs
@@ -0,0 +1,37 @@
+﻿using AppMountComputer.Banco;
+using AppMountComputer.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMountComputer.Paginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CadastroProcessador : ContentPage
+	{
+		public CadastroProcessador ()
+		{
+			InitializeComponent ();
+		}
+
+        public void salvarAction(object Sender, EventArgs args)
+        {
+            Processador p = new Processador();
+            p.marca = marca.Text;
+            p.linha = linha.Text;
+            p.modelo = modelo.Text;
+            p.preco = double.Parse(preco.Text);
+
+            Database database = new Database();
+            database.Cadastro(p);
+
+            App.Current.MainPage =
+                new NavigationPage(new ConsultaProcessador());
+        }
+	}
+}
---
+++ AppMountComputer/AppMountComputer/Paginas/ConsultaProcessador.xaml.cs
@@ -0,0 +1,53 @@
+﻿using AppMountComputer.Banco;
+using AppMountComputer.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMountComputer.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConsultaProcessador : ContentPage
+    {
+        public ConsultaProcessador()
+        {
+            InitializeComponent();
+
+            Database database = new Database();
+            var lista = database.Consultar();
+            ListaProcessadores.ItemsSource = lista;
+
+            lblCount.Text = lista.Count.ToString();
+        }
+
+        public void GoCadastro(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new CadastroProcessador());
+        }
+
+        public void GoMinhasPecas(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new MinhasPecas());
+        }
+
+        public void MaisDetalheAction(object sender, EventArgs args)
+        {
+            Label lblDetalhe = (Label)sender;
+            TapGestureRecognizer tapGest =
+           (TapGestureRecognizer)lblDetalhe.GestureRecognizers[0];
+            Processador processador = tapGest.
+            CommandParameter as Processador;
+            Navigation.PushAsync(new DetalhePeca(processador));
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
